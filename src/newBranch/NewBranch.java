745762a4c525cea79eef66e48576c47fe24b750c
lutzy <artrem.lucyak@bk.ru>
2023-03-31 23:30:56 +0300

    так вот как коммит работает

---
+++ src/newBranch/NewBranch.java
@@ -0,0 +1,10 @@
+package newBranch;
+
+public class NewBranch {
+    public static void main(String[] args) {
+
+    }
+    public static void method(){
+        System.out.println("htitybt gthj");
+    }
+}
